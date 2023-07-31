74df4ef92d63b07f2f76491ce6fe3244abf14df7
Fran Perez <fran06.pe@gmail.com>
2023-07-31 01:47:48 -0500

    dockerfile and first version of the app working

--- app.go
+++ app.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"net/http"
+)
 
+type Data struct {
+	LocalIp string
+}
+
+//func getLocalIp() (localIp, error) {
+//
+//}
+
+func getIndexHtml(responseWriter http.ResponseWriter, request *http.Request) {
+	template, err := template.ParseFiles("templates/index.html")
+	user := Data{"10.10.0.0/32"}
+	//fmt.Println(user)
+	if err != nil {
+		panic(err)
+	} else {
+		template.Execute(responseWriter, user)
+	}
+
+}
 func main() {
-	fmt.Println("Hola mundo")
+	http.HandleFunc("/", getIndexHtml)
+	// Initialize web server on port 8080 without error handler
+	http.ListenAndServe(":8080", nil)
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module go-webapp-cicd
 
 go 1.20
+
+require (
+
+)
