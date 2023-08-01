d566ebfa7d44a6c7ef0579d70d198cd192b74f78
Fran Perez <fran06.pe@gmail.com>
2023-08-01 18:53:41 -0500

    log added

--- app.go
+++ app.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"html/template"
+	"io"
+	"log"
+
 	//"io/ioutil"
 	"net/http"
 )
@@ -19,42 +22,42 @@ type Network struct {
 }
 
 type Data struct {
-	localIp string
+	LocalIp string
 }
 
-//func getEcsMetadata() string {
-//	response, err := http.Get("http://169.254.170.2/v2/metadata")
-//	if err != nil {
-//		panic(err)
-//	}
-//	defer response.Body.Close()
-//	body, err := io.ReadAll(response.Body)
-//
-//	if err != nil {
-//		panic(err)
-//	}
-//	var metadata taskMetadata
-//	if err = json.Unmarshal(body, &metadata); err != nil {
-//		panic(err)
-//	}
-//
-//	if len(metadata.Containers) > 0 && len(metadata.Containers[0].Networks) > 0 && len(metadata.Containers[0].Networks[0].ipv4Address) > 0 {
-//		return metadata.Containers[0].Networks[0].ipv4Address[0]
-//	} else {
-//		return "NoValue"
-//	}
-//}
+func getEcsMetadata() string {
+	response, err := http.Get("http://169.254.170.2/v2/metadata")
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+	body, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		panic(err)
+	}
+	var metadata taskMetadata
+	if err = json.Unmarshal(body, &metadata); err != nil {
+		panic(err)
+	}
+
+	if len(metadata.Containers) > 0 && len(metadata.Containers[0].Networks) > 0 && len(metadata.Containers[0].Networks[0].ipv4Address) > 0 {
+		return metadata.Containers[0].Networks[0].ipv4Address[0]
+	} else {
+		return "NoValue"
+	}
+}
 
 func getIndexHtml(responseWriter http.ResponseWriter, request *http.Request) {
 	template, err := template.ParseFiles("templates/index.html")
-	//LocalIp := Data{getEcsMetadata()}
-	LocalIp := Data{"10.10.0.0/22"}
-	fmt.Println(LocalIp)
+	localIp := Data{getEcsMetadata()}
+	//localIp := Data{"10.10.0.0/22"}
+	log.Println(localIp)
 
 	if err != nil {
 		panic(err)
 	} else {
-		template.Execute(responseWriter, LocalIp)
+		template.Execute(responseWriter, localIp)
 	}
 
 }
